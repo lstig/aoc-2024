0525beef5a61296edad5bf0f84627414d01d01f9
Luke Stigdon <lstig@users.noreply.github.com>
2024-12-04 10:03:45 -0700

    initial day 02

---
+++ 02/src/main.rs
@@ -0,0 +1,48 @@
+fn main() {
+    let input = include_str!("input.txt");
+
+    println!("Part 1: {}", part1(input));
+    println!("Part 2: {}", part2(input));
+}
+
+fn part1(input: &str) -> u32 {
+    0
+}
+
+fn part2(input: &str) -> u32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use indoc::indoc;
+
+    #[test]
+    fn check_part1() {
+        let input = indoc! {"
+            7 6 4 2 1
+            1 2 7 8 9
+            9 7 6 2 1
+            1 3 2 4 5
+            8 6 4 4 1
+            1 3 6 7 9"
+        };
+        let result = part1(input);
+        assert_eq!(result, 2)
+    }
+
+    #[test]
+    fn check_part2() {
+        let input = indoc! {"
+            7 6 4 2 1
+            1 2 7 8 9
+            9 7 6 2 1
+            1 3 2 4 5
+            8 6 4 4 1
+            1 3 6 7 9"
+        };
+        let result = part2(input);
+        assert_eq!(result, 0)
+    }
+}
